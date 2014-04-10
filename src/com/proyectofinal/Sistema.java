19e9d4567a0ccc549fd8cd2389a49efdbe7b59ac
JostinSS <justinsencion@hotmail.com>
2014-04-09 22:20:17 -0600

    probando...

--- src/com/proyectofinal/Sistema.java
+++ src/com/proyectofinal/Sistema.java
@@ -20,7 +20,7 @@ public class Sistema {
 		admin.setLocationRelativeTo(null);
 		admin.setVisible(true);
 		System.out.println("No te apure Lewdin");
-		
+		System.out.println("No te apure Lewdin");
 	/*	MantenimientoClientes m = new MantenimientoClientes();
 		m.setLocationRelativeTo(null);
 		m.setVisible(true);
